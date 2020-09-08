8d5eb4650b2d24a59aa3d6d69629b0b01f76181a
imataka7 <imataka7@gmail.com>
2020-09-09 00:02:23 +0900

    playing

--- react/hooks/src/App.tsx
+++ react/hooks/src/App.tsx
@@ -1,13 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+// const a = () => console.log('Everyday is still day one.');
+// const changeTitle = () => {
+//   document.title = `Updated ${++cnt} times.`;
+// };
+// let cnt = 0;
+let counter: NodeJS.Timer | null = null;
+let f: any;
+
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
+  // f = setCount;
+
+  // console.log('ohayou');
+  // useEffect(() => a());
+  // useEffect(() => console.log('goodnight'));
+
+  // changeTitle();
+  // useEffect(changeTitle);
+  // useEffect(() => changeTitle());
+  useEffect(() => {
+    // document.title = `Updated ${++cnt} times.`;
+    document.title = `Updated ${count} times.`;
+    // console.log(cnt, count);
+    // setCount(cnt);
+    // Object.defineProperty(window, 'setCount', { value: setCount });
+    // f = setCount;
+  });
+
+  if (counter === null) {
+    // Object.defineProperty(window, 'setCount', { value: setCount });
+
+    counter = setInterval(() => {
+      // setCount(count + 1);
+      f?.(count + 1);
+      // setCount(cnt);
+      // cnt++;
+      // console.log('set ', cnt, count, setCount);
+      console.log(count, f);
+    }, 1000);
+  }
 
   return (
     <div className="App">
       <p>You clicked {count} times.</p>
+      {/* <p>{cnt}</p> */}
       <button onClick={() => setCount(count + 1)}>
         click me
       </button>
