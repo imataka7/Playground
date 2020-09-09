b766017f1427fa4e8eef6ecce45348b9518f0fe3
imataka7 <imataka7@gmail.com>
2020-09-09 22:19:24 +0900

    playing

--- react/hooks/src/App.tsx
+++ react/hooks/src/App.tsx
@@ -1,14 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+
+const NameContext = React.createContext('imataka7');
 
 // const a = () => console.log('Everyday is still day one.');
 // const changeTitle = () => {
 //   document.title = `Updated ${++cnt} times.`;
 // };
 // let cnt = 0;
-let counter: NodeJS.Timer | null = null;
-let f: any;
+// let counter: number;
+// let f: any;
+
+const PrintName = () => {
+  const name = useContext(NameContext);
+
+  return <p>{name}</p>;
+};
 
 const App: React.FC = () => {
   const [count, setCount] = useState(0);
@@ -30,45 +38,49 @@ const App: React.FC = () => {
     // f = setCount;
   });
 
-  if (counter === null) {
-    // Object.defineProperty(window, 'setCount', { value: setCount });
+  // if (counter === null) {
+  //   // Object.defineProperty(window, 'setCount', { value: setCount });
 
-    counter = setInterval(() => {
-      // setCount(count + 1);
-      f?.(count + 1);
-      // setCount(cnt);
-      // cnt++;
-      // console.log('set ', cnt, count, setCount);
-      console.log(count, f);
-    }, 1000);
-  }
+  //   counter = window.setInterval(() => {
+  //     // setCount(count + 1);
+  //     f?.(count + 1);
+  //     // setCount(cnt);
+  //     // cnt++;
+  //     // console.log('set ', cnt, count, setCount);
+  //     console.log(count, f);
+  //   }, 1000);
+  // }
 
   return (
-    <div className="App">
-      <p>You clicked {count} times.</p>
-      {/* <p>{cnt}</p> */}
-      <button onClick={() => setCount(count + 1)}>
-        click me
+    <NameContext.Provider value="Takaya Imagawa">
+      <div className="App">
+        <p>You clicked {count} times.</p>
+        {/* <p>{cnt}</p> */}
+        <button onClick={() => setCount(count + 1)}>
+          click me
       </button>
 
-      <button onClick={() => setCount(0)}>
-        reset
+        <button onClick={() => setCount(0)}>
+          reset
       </button>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+            </a>
+        </header> */}
+
+        <PrintName></PrintName>
+      </div>
+    </NameContext.Provider>
   );
 };
 
