e56a7febce677e9cdeb62ef407bb0394531f5204
imataka7 <imataka7@gmail.com>
2020-09-14 20:04:33 +0900

    Add next-blog

--- react/t/src/App.tsx
+++ react/t/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let a: symbol = Symbol('aon');
   return (
     <div className="App">
       <header className="App-header">
